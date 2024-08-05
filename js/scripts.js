6201651dd0262ecb5bdeda17a11bf682bc80acef
Charls <carlos.mancilla993@gmail.com>
2024-08-05 18:23:39 -0500

    comunicados carousel nav

--- js/scripts.js
+++ js/scripts.js
@@ -96,6 +96,7 @@
             autoplay: false,
             margin: 20,
             dots: true,
+            nav: true,
             responsive: {
                 0: {
                     items: 1,
